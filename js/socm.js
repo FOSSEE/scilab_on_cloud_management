3b3e97523f018177fbcf22d8dac59d464ee29843
prashantsinalkar <prashantsinalkar@gmail.com>
2018-01-23 17:59:09 +0530

    added chapter / example display settings

--- js/socm.js
+++ js/socm.js
@@ -3,16 +3,40 @@
 
 		var basePath = Drupal.settings.basePath;
 		var modPath1 = basePath + "scilab-on-cloud-management/display-books/";
-		$Selected = $(".selected");
-		$Selected.click(function(e) {
+		$Selected_book = $(".selected-book");
+		$Selected_chapter = $(".selected-chapter");
+		$Selected_example = $(".selected-example");
+
+		$Selected_book.click(function(e) {
 			$(".sync-msg").remove();
 			$(this).after("<span class='sync-msg'>Saving...</span>");
 			$.ajax({
-				url: modPath1 + "ajax/selected/" + $(this).attr("data-bid"),
+				url: modPath1 + "ajax/book/" + $(this).attr("data-bid"),
 				success: function(data) {
 					$(".sync-msg").remove();
 					console.log("success");
-					console.log(data);
+				}
+			});
+		});
+        $Selected_chapter.click(function(e) {
+			$(".sync-msg").remove();
+			$(this).after("<span class='sync-msg'>Saving...</span>");
+			$.ajax({
+				url: modPath1 + "ajax/chapter/" + $(this).attr("data-bid"),
+				success: function(data) {
+					$(".sync-msg").remove();
+					console.log("success");
+				}
+			});
+		});
+        $Selected_example.click(function(e) {
+			$(".sync-msg").remove();
+			$(this).after("<span class='sync-msg'>Saving...</span>");
+			$.ajax({
+				url: modPath1 + "ajax/example/" + $(this).attr("data-bid"),
+				success: function(data) {
+					$(".sync-msg").remove();
+					console.log("success");
 				}
 			});
 		});
